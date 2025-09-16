e47e2cac8e63f7914fdf8d4a8c34faa5df4f34fe
Caleb Bowen <cdb0059@uah.edu>
2025-09-16 09:34:31 -0500

    couple quick changes

--- Python_DataBase_Interface/database_to_csv.py
+++ Python_DataBase_Interface/database_to_csv.py
@@ -6,17 +6,8 @@ import csv
 conn = sqlite3.connect("SQLITE_DataBase/comments.db")
 cur = conn.cursor()
 
-# Load schema from external file
 project_directory = Path.cwd()
-file_path = project_directory/"SQLITE_DataBase"/"schema.sql"
-with open(file_path, "r", encoding="utf-8") as f:
-    schema_sql = f.read()
-
-# Path to the output CSV file
 output_csv_file = project_directory/"trainingData_csv"/"comments_export.csv"
-
-# Execute the schema (can contain multiple statements)
-cur.executescript(schema_sql)
 
 # Export comments to CSV
 cur.execute("SELECT id, platform, source_id, author_id, text, published_at, like_count, lang FROM comment")
@@ -30,5 +21,4 @@ with open(output_csv_file, "w", newline="", encoding="utf-8") as csvfile:
     # Write data rows
     csv_writer.writerows(rows)
 
-conn.commit()
-conn.close()    
+conn.close()
